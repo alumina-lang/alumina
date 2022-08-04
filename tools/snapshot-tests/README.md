ec994214df3e5c53f655876e585cfdacd66f316e
Tibor Djurica Potpara <tibor.djurica@ojdip.net>
2022-08-04 01:22:48 +0100

    fix: Get rid of malloc in thread_init

--- tools/snapshot-tests/README.md
+++
@@ -1,3 +0,0 @@
-# snapshot-tests
-
-In the absence of a proper test suite, this tool provides snapshot tests of the `./examples` folder to catch regressions in the compiler and standard library.
--- tools/snapshot-tests/monkeypatch.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-#include <sys/types.h>
-
-ssize_t getrandom_mock(void *buf, size_t buflen, unsigned int flags) {
-    for (int i = 0; i < buflen; ++i) {
-        ((unsigned char*)buf)[i] = (unsigned char)(0x7f + i);
-    }
-    return buflen;
-}
